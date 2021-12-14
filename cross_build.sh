299b1fb6ad570cd6d43b0383b3cb2abd3b0a5d52
kyuta3579 <kohei.fukunaga@qtasrv.net>
2021-12-14 11:52:47 +0900

    [dev-env]fix Docker user and directory.

--- cross_build.sh
+++ cross_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# docker build --platform linux/amd64 -t enviro-cross-rpz ./env/
-docker run -it -v "$PWD":/Users/kohei/Documents/dev/enviro_play -e CARGO_HOME="$PWD"/.docker-cargo enviro-cross-rpz
+# docker build -t enviro-cross-rpz ./env/
+docker run -it --rm -v "$PWD":/Enviro-By-Rust -e CARGO_HOME=/Enviro-By-Rust/.docker-cargo enviro-cross-rpz
