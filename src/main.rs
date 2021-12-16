c4ea0a38c510effb42b25c698ada9998a68cc205
kyuta3579@yahoo.co.jp <kohei.fukunaga@qtasrv.net>
2021-12-16 23:57:01 +0900

    振幅見て出した.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use linux_embedded_hal::{Delay, I2cdev};
 use bme280::BME280;
 use ltr_559::{Ltr559, SlaveAddr, AlsGain, AlsIntTime, AlsMeasRate};
-use alsa::{Direction, ValueOr};
+use alsa::{Direction, ValueOr, Error};
 use alsa::pcm::{PCM, HwParams, Format, Access, State};
-use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
-use std::error::Error;
+use std::ffi::{CString, CStr};
+
 use rppal::gpio::Gpio;
 
 fn main() {
-
-    let pcm = PCM::new("plughw:0", Direction::Capture, true).unwrap();
+    let devicename = CString::new("plughw:0").unwrap();
+    let pcm = PCM::open(&devicename, Direction::Capture, false).unwrap();
 
     let hwp = HwParams::any(&pcm).unwrap();
     hwp.set_channels(1).unwrap();
     hwp.set_rate(48000, ValueOr::Nearest).unwrap();
-    hwp.set_format(Format::float()).unwrap();
+    hwp.set_format(Format::s32()).unwrap();
     hwp.set_access(Access::RWInterleaved).unwrap();
     pcm.hw_params(&hwp).unwrap();
 
-    let io = pcm.io_f32().unwrap();
+    let io = pcm.io_i32().unwrap();
 
     let i2c_bme280 = I2cdev::new("/dev/i2c-1").unwrap();
     let mut bme280 = BME280::new_primary(i2c_bme280, Delay);
@@ -35,7 +35,6 @@ fn main() {
     
     let hwp = pcm.hw_params_current().unwrap();
     let swp = pcm.sw_params_current().unwrap();
-    swp.set_start_threshold(hwp.get_buffer_size().unwrap()).unwrap();
     pcm.sw_params(&swp).unwrap();
 
     loop {
@@ -53,15 +52,36 @@ fn main() {
         println!("Temperature = {} deg C", measurements.temperature);
         println!("Pressure = {} pascals", measurements.pressure);
 
-        let mut soundbuf = [0f32; 1024];
-        let ret = io.readi(&mut soundbuf[..]);
+        {
+            let mut soundbuf = [0i32; 8192];
+            let mut ret: Result<usize, Error> = Result::Ok(0);
+            if pcm.state() != State::Running {
+                match pcm.start() {
+                    Ok(())  => {
+                        println!("start pcm mic.");
+                        ret = io.readi(&mut soundbuf[..]);
+                    },
+                    Err(err) => println!("error start pcm mic.{:?}", err),
+                };
+            } else {
+                ret = io.readi(&mut soundbuf[..]);
+            }
 
-        if match ret
-        let res = samples_fft_to_spectrum(
-            &soundbuf[..],
-            48000,
-            FrequencyLimit::All,
-            Some(&|val, info| val - info.min),
-        );
+            match ret {
+                Ok(size) => {
+                    println!("read size is {}", size);
+                    let mut max = 0;
+                    for amplitude in soundbuf.iter() {
+                        if max < amplitude.abs() {
+                            max = amplitude.abs()
+                        }
+                    }
+                    println!("amplitude: {}", max / 1000000);
+                },
+                Err(err) => {
+                    println!("no input.{:?}", err);
+                },
+            };
+        }
     }
 }
